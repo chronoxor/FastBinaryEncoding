d651695a4a7daaee808fdf5973ea8196c949c643
Ivan Shynkarenka <chronoxor@gmail.com>
2018-08-22 20:35:47 +0300

    update

--- performance/verify_final.cpp
+++ performance/verify_final.cpp
@@ -14,7 +14,7 @@ protected:
     FinalVerifyFixture()
     {
         // Create a new account with some orders
-        proto::Account account = { 1, "Test", proto::State::good, { "USD", 1000.0 }, std::make_optional<proto::Balance>({ "EUR", 100.0 }), {} };
+        proto::Account account = { 1, "Test", proto::State::good, { "USD", 1000.0 }, stdmakeoptional<proto::Balance>({ "EUR", 100.0 }), {} };
         account.orders.emplace_back(1, "EURUSD", proto::OrderSide::buy, proto::OrderType::market, 1.23456, 1000.0);
         account.orders.emplace_back(2, "EURUSD", proto::OrderSide::sell, proto::OrderType::limit, 1.0, 100.0);
         account.orders.emplace_back(3, "EURUSD", proto::OrderSide::buy, proto::OrderType::stop, 1.5, 10.0);
