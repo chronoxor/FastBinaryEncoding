59c33435b35a988e66c3de37130f4c797796cb31
Travis CI <travis@travis-ci.org>
2018-09-26 23:22:00 +0000

    Update generated documentation

--- search/functions_a.js
+++ search/functions_a.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['setarraysize',['SetArraySize',['../struct_f_b_e_1_1_struct_field.html#a37590ebb3e5bb54b86245597100a9154',1,'FBE::StructField']]],
   ['structfield',['StructField',['../struct_f_b_e_1_1_struct_field.html#a7902f3995234736f6f61718be50d6b7e',1,'FBE::StructField']]],
-  ['structtype',['StructType',['../struct_f_b_e_1_1_struct_type.html#abe2e58bd721ed0e1df3b4070445c38ec',1,'FBE::StructType']]]
+  ['structtype',['StructType',['../struct_f_b_e_1_1_struct_type.html#abe2e58bd721ed0e1df3b4070445c38ec',1,'FBE::StructType']]],
+  ['switch',['switch',['../fbe-parser_8cpp.html#ada8aa24d50028bab4e2a4437e6eb7807',1,'fbe-parser.cpp']]]
 ];
