101f9382ed1761900afac9ad55fa34bf0a8c71f4
Ivan Shynkarenka <chronoxor@gmail.com>
2020-10-07 19:33:13 +0300

    build

--- modules/cpp-optparse.cmake
+++ modules/cpp-optparse.cmake
@@ -2,16 +2,16 @@ if(NOT TARGET cpp-optparse)
 
   # Module library
   file(GLOB SOURCE_FILES "cpp-optparse/OptionParser.cpp")
-  if(NOT MSVC)
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
-  else()
-    # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
-    set_source_files_properties(${SOURCE_FILES} PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4244")
-  endif()
   add_library(cpp-optparse ${SOURCE_FILES})
+  if(MSVC)
+    # C4244: 'conversion' conversion from 'type1' to 'type2', possible loss of data
+    set_target_properties(cpp-optparse PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS} /wd4244")
+  else()
+    set_target_properties(cpp-optparse PROPERTIES COMPILE_FLAGS "${PEDANTIC_COMPILE_FLAGS}")
+  endif()
   target_include_directories(cpp-optparse PUBLIC "cpp-optparse")
 
   # Module folder
-  set_target_properties(cpp-optparse PROPERTIES FOLDER modules/cpp-optparse)
+  set_target_properties(cpp-optparse PROPERTIES FOLDER "modules/cpp-optparse")
 
 endif()
