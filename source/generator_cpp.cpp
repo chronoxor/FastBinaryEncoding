88705eaf44e58ff6a313e6ad32a62f25777142ba
Ivan Shynkarenka <chronoxor@gmail.com>
2021-04-29 18:49:35 +0300

    Fixed rapidjson GetObject() issue

--- source/generator_cpp.cpp
+++ source/generator_cpp.cpp
@@ -6234,7 +6234,7 @@ struct ChildNodeReader
             return false;
 
         // Deserialize the child object
-        return FBE::JSON::from_json(member->value.GetObject(), value);
+        return FBE::JSON::from_json(member->value.GetObj(), value);
     }
 };
 
